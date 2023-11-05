9c9a129d2c4129d33f7cedd13580455a4066e999
DanielPRe15 <dani123pre@gmail.com>
2023-11-05 18:10:38 -0500

    Agregar archivos de proyecto.

---
+++ SistemaFerrreteria/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SistemaFerrreteria.MvcApplication" Language="C#" %>
---
+++ SistemaFerrreteria/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaFerrreteria.Startup))]
+namespace SistemaFerrreteria
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
